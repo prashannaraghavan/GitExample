be135b9e35841feb51d9eef3308f2075a3f11fc6
Manu Sharma <manu.sharma@us.imshealth.com>
2018-12-10 11:16:42 -0700

    Initial Commit

---
+++ Git test projectTests/Git_test_projectTests.swift
@@ -0,0 +1,36 @@
+//
+//  Git_test_projectTests.swift
+//  Git test projectTests
+//
+//  Created by Sharma, Manu on 12/10/18.
+//  Copyright © 2018 IQVIA. All rights reserved.
+//
+
+import XCTest
+@testable import Git_test_project
+
+class Git_test_projectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
