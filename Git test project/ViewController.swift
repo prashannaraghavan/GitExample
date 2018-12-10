7e28aed9b10223820cf56ca512e8dda8f16cd343
Manu Sharma <manu.sharma@us.imshealth.com>
2018-12-10 11:53:59 -0700

    Also adding view will appear

--- Git test project/ViewController.swift
+++ Git test project/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Manage your views here.")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
