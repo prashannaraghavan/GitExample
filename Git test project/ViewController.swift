e47deaf30e1832a9eedaaaf3c8ed1eacde067fd8
Manu Sharma <manu.sharma@us.imshealth.com>
2018-12-17 12:01:06 -0700

    Oops forgot to add this comment

--- Git test project/ViewController.swift
+++ Git test project/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    // Gets called after ViewDidLoad()
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("View controller appears")
